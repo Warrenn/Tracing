a5517c30057b215d11d72bbaf537e87ae0833097
Warrenn <warrenn.enslin@barclayscapital.com>
2017-06-19 17:03:23 +0200

    updated the property function provider

--- testinject/Program.cs
+++ testinject/Program.cs
@@ -11,21 +11,10 @@ namespace testinject
 {
     class Program
     {
-        private static readonly MethodInfo setGenMethodInfo =
-                typeof(PropertyFunctionProvider)
-                .GetMethod("CreateSetPropertyFunctionGeneric");
-
-        private static readonly MethodInfo getGenMethodInfo =
-                typeof(PropertyFunctionProvider)
-                .GetMethod("CreateGetPropertyFunctionGeneric");
-
-        private static readonly MethodInfo elementAtIndexMethodInfo =
-                typeof(Program)
-                .GetMethod("ElementAtIndex");
 
         static void Main(string[] args)
         {
-            var mappings = CreateTypeMappings("Main", typeof(Main), typeof(Main));
+            var mappings = PropertyFunctionProvider.CreateTypeMappings("", typeof(Main), typeof(Main));
             var main = new Main
             {
                 Lots = new List<Sub2>
@@ -34,7 +23,10 @@ namespace testinject
                 }
             };
 
-            SetProperty(main, "Main_Lots_1_Sub2_Subb_2_Sub3_Int", "20", mappings);
+            
+            PropertyFunctionProvider.SetProperty(main, "Lots_1_Subb_2_Int", "20", mappings);
+            PropertyFunctionProvider.SetProperty(main, "Field7", "2017-11-13", mappings);
+
             var next2 = mappings["Main_Sub1_Sub2_Field4"].SetPropertyDelegate;
 
             main = (Main)next2(main, "22");
@@ -46,77 +38,6 @@ namespace testinject
             var next7 = PropertyFunctionProvider.CreateSetPropertyFunction<Main>("Main_Field8");
 
             main = next7(main, "129230,999");
-        }
-
-        private static object SetProperty(object instance, string path, object value,
-            IDictionary<string, PropertyMapping> mappings)
-        {
-            var pattern = @"_(\d+)_";
-            var parts = Regex.Split(path, pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            var partCount = parts.Length/2;
-
-            for (var pathIndex = 0; pathIndex < partCount; pathIndex++)
-            {
-                var propPart = (pathIndex*2);
-                var indexPart = (pathIndex*2) + 1;
-                var part = parts[propPart];
-                var getProperty = mappings[part].GetPropertyDelegate;
-                var setProperty = mappings[part].SetPropertyDelegate;
-                var propType = mappings[part].PropertyType;
-
-                var indexString = parts[indexPart];
-                int arrayIndex;
-                if (!int.TryParse(indexString, out arrayIndex)) return null;
-
-                var collection = getProperty(instance);
-
-                var arrayType = propType.IsArray
-                    ? propType.GetElementType()
-                    : propType.GenericTypeArguments[0];
-
-                var genElementAtIndexMethodInfo = elementAtIndexMethodInfo.MakeGenericMethod(arrayType);
-                var arguments = new[] {collection, arrayIndex};
-                var element = genElementAtIndexMethodInfo.Invoke(null, arguments);
-
-                collection = arguments[0];
-                setProperty(instance, collection);
-
-                instance = element;
-            }
-
-            var lastpart = parts[parts.Length - 1];
-
-            var setPropertyFunc = mappings[lastpart].SetPropertyDelegate;
-
-            return setPropertyFunc(instance, value);
-        }
-
-        public static T ElementAtIndex<T>(ref object instance, int index)
-        {
-            var num = 0;
-            var collection = instance as ICollection<T> ?? new Collection<T>();
-
-            using (var enumerator = collection.GetEnumerator())
-            {
-                while (enumerator.MoveNext())
-                {
-                    if (num == index) return enumerator.Current;
-                    checked { ++num; }
-                }
-            }
-            var type = typeof(T);
-            var createDefault = (type.GetConstructor(Type.EmptyTypes) == null)
-                ? (Func<T>)(() => default(T))
-                : () => (T)Activator.CreateInstance(type);
-            var newItem = createDefault();
-            while (num <= index)
-            {
-                newItem = createDefault();
-                collection.Add(newItem);
-                checked { ++num; }
-            }
-            instance = collection;
-            return newItem;
         }
 
 
@@ -139,53 +60,5 @@ namespace testinject
             indexer.SetValue(collection, value, indexArgs);
         }
 
-
-        public static IDictionary<string, PropertyMapping> CreateTypeMappings(string path, Type coreType, Type type)
-        {
-            var dictionary = new Dictionary<string, PropertyMapping>();
-
-            foreach (var propertyInfo in type.GetProperties())
-            {
-                var propType = propertyInfo.PropertyType;
-                var propName = $"{path}_{propertyInfo.Name}";
-                var setMethodInfo = setGenMethodInfo.MakeGenericMethod(coreType);
-                var getMethodInfo = getGenMethodInfo.MakeGenericMethod(coreType, propType);
-
-                dictionary.Add(propName, new PropertyMapping
-                {
-                    CoreType = coreType,
-                    PropertyType = propType,
-                    SetPropertyDelegate =
-                        (Func<object, object, object>)setMethodInfo
-                            .Invoke(null, new object[] { propName }),
-                    GetPropertyDelegate =
-                        (Func<object, object>)getMethodInfo
-                            .Invoke(null, new object[] { propName })
-                });
-
-                var nextCoreType = coreType;
-
-                if (propType.IsEnum ||
-                    propType.IsValueType ||
-                    typeof(string).IsAssignableFrom(propType))
-                    continue;
-
-                if (typeof(IEnumerable).IsAssignableFrom(propType))
-                {
-                    nextCoreType = propType.IsArray
-                        ? propType.GetElementType()
-                        : propType.GenericTypeArguments[0];
-                    propName = nextCoreType.Name;
-                    propType = nextCoreType;
-                }
-
-                foreach (var typeMapping in CreateTypeMappings(propName, nextCoreType, propType))
-                {
-                    dictionary[typeMapping.Key] = typeMapping.Value;
-                }
-            }
-
-            return dictionary;
-        }
     }
 }
--- testinject/PropertyFunctionProvider.cs
+++ testinject/PropertyFunctionProvider.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace testinject
 {
     public class PropertyFunctionProvider
     {
+        private static readonly MethodInfo SetGenMethodInfo =
+                typeof(PropertyFunctionProvider)
+                .GetMethod("CreateSetPropertyFunctionGeneric");
+
+        private static readonly MethodInfo GetGenMethodInfo =
+                typeof(PropertyFunctionProvider)
+                .GetMethod("CreateGetPropertyFunctionGeneric");
+
+        private static readonly MethodInfo ElementAtIndexMethodInfo =
+                typeof(PropertyFunctionProvider)
+                .GetMethod("ElementAtIndex");
+
         private static IList<Expression> CreateMemberExpression<TClass>(string path, Expression parameter)
         {
             var expressions = new List<Expression>();
@@ -20,7 +36,7 @@ namespace testinject
 
             var parts = path.Split('_');
 
-            for (var i = 1; i < parts.Length - 1; i++)
+            for (var i = 0; i < parts.Length - 1; i++)
             {
                 valueExpression = Expression.PropertyOrField(valueExpression, parts[i]);
                 newExpression = Expression.New(valueExpression.Type);
@@ -39,7 +55,7 @@ namespace testinject
         public static Func<object, object, object> CreateSetPropertyFunctionGeneric<T>(string path)
         {
             var propFunc = CreateSetPropertyFunction<T>(path);
-            return (o, o1) => propFunc((T) o, o1);
+            return (o, o1) => propFunc((T)o, o1);
         }
 
         public static Func<object, object> CreateGetPropertyFunctionGeneric<T, TProp>(string path)
@@ -91,6 +107,138 @@ namespace testinject
             var body = Expression.Block(expressions);
             var expressionTree = Expression.Lambda<Func<TClass, object, TClass>>(body, parameters);
             return expressionTree.Compile();
+        }
+
+        public static void SetProperty(object instance, string path, object value,
+            IDictionary<string, PropertyMapping> mappings)
+        {
+            var pattern = @"_(\d+)_";
+            var parts = Regex.Split(path, pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var partCount = parts.Length / 2;
+
+            for (var pathIndex = 0; pathIndex < partCount; pathIndex++)
+            {
+                var propPart = (pathIndex * 2);
+                var indexPart = (pathIndex * 2) + 1;
+                var part = parts[propPart];
+
+                if (!mappings.ContainsKey(part)) return;
+                var getProperty = mappings[part].GetPropertyDelegate;
+                var setProperty = mappings[part].SetPropertyDelegate;
+                var propType = mappings[part].PropertyType;
+
+                var indexString = parts[indexPart];
+                int arrayIndex;
+                if (!int.TryParse(indexString, out arrayIndex)) return;
+                arrayIndex--;
+
+                var collection = getProperty(instance);
+
+                var arrayType = propType.IsArray
+                    ? propType.GetElementType()
+                    : propType.GenericTypeArguments[0];
+
+                var genElementAtIndexMethodInfo = ElementAtIndexMethodInfo.MakeGenericMethod(arrayType);
+                var arguments = new[] { collection, arrayIndex };
+                var element = genElementAtIndexMethodInfo.Invoke(null, arguments);
+
+                collection = arguments[0];
+                setProperty(instance, collection);
+
+                instance = element;
+            }
+
+            var lastpart = parts[parts.Length - 1];
+            if (!mappings.ContainsKey(lastpart)) return;
+
+            var setPropertyFunc = mappings[lastpart].SetPropertyDelegate;
+
+            setPropertyFunc(instance, value);
+        }
+
+        public static T ElementAtIndex<T>(ref object instance, int index)
+        {
+            var num = 0;
+            var collection = instance as ICollection<T> ?? new Collection<T>();
+
+            using (var enumerator = collection.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    if (num == index) return enumerator.Current;
+                    checked { ++num; }
+                }
+            }
+            var type = typeof(T);
+            var createDefault = (type.GetConstructor(Type.EmptyTypes) == null)
+                ? (Func<T>)(() => default(T))
+                : () => (T)Activator.CreateInstance(type);
+            var newItem = createDefault();
+            while (num <= index)
+            {
+                newItem = createDefault();
+                collection.Add(newItem);
+                checked { ++num; }
+            }
+            instance = collection;
+            return newItem;
+        }
+
+        public static IDictionary<string, PropertyMapping> CreateTypeMappings(string path, Type coreType, Type type)
+        {
+            var dictionary = new Dictionary<string, PropertyMapping>();
+            path = string.IsNullOrEmpty(path) ? string.Empty : $"{path}_";
+
+            foreach (var propertyInfo in type.GetProperties())
+            {
+                var propType = propertyInfo.PropertyType;
+                var propName = $"{path}{propertyInfo.Name}";
+                var setMethodInfo = SetGenMethodInfo.MakeGenericMethod(coreType);
+                var getMethodInfo = GetGenMethodInfo.MakeGenericMethod(coreType, propType);
+
+                dictionary.Add(propName, new PropertyMapping
+                {
+                    CoreType = coreType,
+                    PropertyType = propType,
+                    SetPropertyDelegate =
+                        (Func<object, object, object>)setMethodInfo
+                            .Invoke(null, new object[] { propName }),
+                    GetPropertyDelegate =
+                        (Func<object, object>)getMethodInfo
+                            .Invoke(null, new object[] { propName })
+                });
+
+                var nextCoreType = coreType;
+
+                if (propType.IsEnum ||
+                    propType.IsValueType ||
+                    typeof(string).IsAssignableFrom(propType))
+                    continue;
+
+                if (typeof(IEnumerable).IsAssignableFrom(propType))
+                {
+                    nextCoreType = propType.IsArray
+                        ? propType.GetElementType()
+                        : propType.GenericTypeArguments[0];
+                    propName = string.Empty;
+                    propType = nextCoreType;
+                }
+
+                foreach (var typeMapping in CreateTypeMappings(propName, nextCoreType, propType))
+                {
+                    dictionary[typeMapping.Key] = typeMapping.Value;
+                }
+            }
+
+            return dictionary;
+        }
+
+        public class PropertyMapping
+        {
+            public Func<object, object, object> SetPropertyDelegate { get; set; }
+            public Func<object, object> GetPropertyDelegate { get; set; }
+            public Type PropertyType { get; set; }
+            public Type CoreType { get; set; }
         }
     }
 }
